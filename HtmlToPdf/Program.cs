3122b3a70c1269b71757d6954460a40e5b8906d4
Nikita Shah <nikitashah@rangamindia.com>
2016-12-12 08:04:48 -0500

    Added my project

---
+++ HtmlToPdf/Program.cs
@@ -0,0 +1,111 @@
+﻿using HtmlAgilityPack;
+using HTMLtoPDF;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HtmlToPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] pdf; // result will be here
+        
+            var cssText = File.ReadAllText("layout.css");
+            var html = File.ReadAllText("demo.html");
+
+            html = AutoCloseHtmlTags(html);
+
+
+            using (var memoryStream = new MemoryStream())
+            {
+                var document = new Document(PageSize.A4, 0, 0, 0, 0);
+                var writer = PdfWriter.GetInstance(document, memoryStream);
+                document.Open();
+
+                using (var cssMemoryStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(cssText)))
+                {
+                    using (var htmlMemoryStream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(html)))
+                    {
+                        XMLWorkerHelper.GetInstance().ParseXHtml(writer, document, htmlMemoryStream, cssMemoryStream);
+                    }
+                }
+
+                document.Close();
+
+                pdf = memoryStream.ToArray();
+                File.WriteAllBytes("Foo.pdf", pdf);
+
+            }
+        }
+
+
+        public static void CreatePDFFromHTMLFile(string HtmlStream, string FileName)
+        {
+            try
+            {
+                object TargetFile = FileName;
+                string ModifiedFileName = string.Empty;
+                string FinalFileName = string.Empty;
+
+
+                GeneratePDF.HtmlToPdfBuilder builder = new GeneratePDF.HtmlToPdfBuilder(iTextSharp.text.PageSize.A4);
+                GeneratePDF.HtmlPdfPage first = builder.AddPage();
+                first.AppendHtml(HtmlStream);
+                byte[] file = builder.RenderPdf();
+                File.WriteAllBytes(TargetFile.ToString(), file);
+
+                iTextSharp.text.pdf.PdfReader reader = new iTextSharp.text.pdf.PdfReader(TargetFile.ToString());
+                ModifiedFileName = TargetFile.ToString();
+                ModifiedFileName = ModifiedFileName.Insert(ModifiedFileName.Length - 4, "1");
+
+                iTextSharp.text.pdf.PdfEncryptor.Encrypt(reader, new FileStream(ModifiedFileName, FileMode.Append), iTextSharp.text.pdf.PdfWriter.STRENGTH128BITS, "", "", iTextSharp.text.pdf.PdfWriter.AllowPrinting);
+                reader.Close();
+                if (File.Exists(TargetFile.ToString()))
+                    File.Delete(TargetFile.ToString());
+                FinalFileName = ModifiedFileName.Remove(ModifiedFileName.Length - 5, 1);
+                //File.Copy(ModifiedFileName, FinalFileName);
+                //if (File.Exists(ModifiedFileName))
+                //    File.Delete(ModifiedFileName);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static string AutoCloseHtmlTags(string inputHtml)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.OptionWriteEmptyNodes = true;
+            htmlDoc.LoadHtml(inputHtml);
+
+            var inputTags = htmlDoc.DocumentNode.SelectNodes("//input");
+            HtmlNode span = htmlDoc.CreateElement("span");
+            HtmlNode node2;
+            HtmlAttribute att;
+            //foreach (HtmlNode input in inputTags)
+            //{
+
+            //    att = input.Attributes["value"];
+            //    span.InnerHtml = att.Value.ToString();
+            //    node2 = input.ParentNode;
+            //    node2.AppendChild(span);
+            //    node2.RemoveChild(input);
+
+            //}
+            return htmlDoc.DocumentNode.OuterHtml;
+        }
+
+    }
+}
+
---
+++ HtmlToPdf/StringExtensions.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlToPdf
+{
+    public static class StringExtensions
+    {
+        public static string CloseTags(this string html)
+        {
+            if (String.IsNullOrWhiteSpace(html))
+            {
+                return html;
+            }
+
+            return new TagCloser().CloseTags(html);
+        }
+
+        class TagCloser
+        {
+            private Stack<string> _openTags = new Stack<string>();
+            private string _html;
+            private int _currentCharIndex;
+            private bool Eof
+            {
+                get { return _currentCharIndex == _html.Length - 1; }
+            }
+
+            private int _pendingTagNameStartIndex;
+            private string _pendingTagName;
+            private int _currentEndTagStartIndex;
+
+            private State _state;
+
+            public string CloseTags(string html)
+            {
+                _html = html;
+                _currentCharIndex = -1;
+
+                while (!Eof)
+                {
+                    var ch = ReadNext();
+
+                    if (_state == State.InTagName)
+                    {
+                        // Self closing
+                        if (ch == '/' && PeekNext() == '>')
+                        {
+                            ReadNext();
+                            _state = _openTags.Count > 0 ? State.InsideTag : State.None;
+                            continue;
+                        }
+
+                        if (ch == ' ' || ch == '>')
+                        {
+                            _pendingTagName = _html.Substring(_pendingTagNameStartIndex, _currentCharIndex - _pendingTagNameStartIndex);
+                        }
+
+                        if (ch == ' ')
+                        {
+                            _state = State.InAttributes;
+                        }
+                        else if (ch == '>')
+                        {
+                            _openTags.Push(_pendingTagName);
+                            _state = State.InsideTag;
+                        }
+                    }
+                    else if (_state == State.InsideTag)
+                    {
+                        if (ch == '<' && PeekNext() == '/')
+                        {
+                            _currentEndTagStartIndex = _currentCharIndex;
+                            ReadNext();
+                            _state = State.InEndTag;
+                            continue;
+                        }
+                        if (ch == '<')
+                        {
+                            _state = State.InTagName;
+                            _pendingTagNameStartIndex = _currentCharIndex + 1;
+                            continue;
+                        }
+                    }
+                    else if (_state == State.InEndTag)
+                    {
+                        if (ch == '>')
+                        {
+                            _openTags.Pop();
+                            _state = _openTags.Count > 0 ? State.InsideTag : State.None;
+                        }
+                    }
+                    else
+                    {
+                        if (ch == '<')
+                        {
+                            _state = State.InTagName;
+                            _pendingTagNameStartIndex = _currentCharIndex + 1;
+                            continue;
+                        }
+                    }
+                }
+
+                // Broken start tag
+                if (_state == State.InTagName || _state == State.InAttributes)
+                {
+                    _html = _html.Substring(0, _pendingTagNameStartIndex - 1);
+                }
+                // Broken end tag
+                else if (_state == State.InEndTag)
+                {
+                    _html = _html.Substring(0, _currentEndTagStartIndex);
+                }
+
+                var sb = new StringBuilder();
+                sb.Append(_html);
+
+                while (_openTags.Count > 0)
+                {
+                    var tag = _openTags.Pop();
+                    sb.Append("</" + tag + ">");
+                }
+
+                return sb.ToString();
+            }
+
+            private char ReadNext()
+            {
+                return Eof ? '\0' : _html[++_currentCharIndex];
+            }
+
+            private char PeekNext()
+            {
+                return Eof ? '\0' : _html[_currentCharIndex + 1];
+            }
+
+            enum State
+            {
+                None, InTagName, InAttributes, InsideTag, InEndTag
+            }
+        }
+    }
+}
